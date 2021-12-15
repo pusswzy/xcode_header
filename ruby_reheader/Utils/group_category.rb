e832460e295713cd3af6284d138abe79ccc7dcbc
pusswzy <499272000@qq.com>
2021-12-15 20:43:37 +0800

    feat: 生成project下的所有target的hmap

--- ruby_reheader/Utils/group_category.rb
+++ ruby_reheader/Utils/group_category.rb
@@ -8,11 +8,13 @@ module Xcodeproj
         #获取当前group的header_map
         def fetch_public_path_map_file
           header_map = Hash.new
+          puts "self.groupName:#{self.display_name}"
           self.recursive_children.each do |child|
             if child.respond_to? "last_known_file_type"
               if child.last_known_file_type == "sourcecode.c.h"
+	              puts child.display_name + '✨' + child.path
                 header_path = Pathname(child.real_path)
-                header_map.store(File.basename(header_path), File.dirname(header_path))
+                header_map.store(File.basename(header_path).to_s, header_path.to_s)
               end
             end
           end
